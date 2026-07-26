5e5f71952a7be66be0788a962a96c6cb83bf84f5
wzp <1239263644@qq.com>
2026-07-27 00:48:35 +0800

    添加unp

--- 11-UNP-Vol1/code/unpv13e/server/pthread07.h
+++
@@ -1,9 +0,0 @@
-typedef struct {
-  pthread_t		thread_tid;		/* thread ID */
-  long			thread_count;	/* # connections handled */
-} Thread;
-extern Thread	*tptr;		/* array of Thread structures; calloc'ed */
-
-extern int				listenfd, nthreads;
-extern socklen_t		addrlen;
-extern pthread_mutex_t	mlock;
